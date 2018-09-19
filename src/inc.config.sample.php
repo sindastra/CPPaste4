1c114fa7f20f3fbe04500afc2d6636e07a42cb63
sindastra <1236984+sindastra@users.noreply.github.com>
2018-09-19 22:25:33 +0200

    Added sample config file and added the real config file to .gitignore!

---
+++ src/inc.config.sample.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * CPPaste4
+ *
+ * Copyright (c) 2015 - 2018 Sindastra <https://github.com/sindastra>
+ * All rights reserved.
+ *
+ * The above copyright notice and this notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * See LICENSE file for more info.
+ *
+ * @author Sindastra <https://github.com/sindastra>
+ * @copyright (c) 2015 - 2018 Sindastra <https://github.com/sindastra>
+ */
+
+// Set the parameters below and rename
+// this file by removing the '.sample'!
+
+$conf = array(
+	'PrettyRewrite' => false
+);
